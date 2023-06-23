1e4e621b35adbeecc108f0ace310e777bdef2f8d
Kwadwo Asubonteng <kasubonteng2002@gmail.com>
2023-06-23 23:19:38 +0000

    completed layout

---
+++ components/Box.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
+interface BoxProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ className, children }) => {
+	return (
+		<div
+			className={twMerge(` bg-neutral-900 rounded-lg h-fit w-full`, className)}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default Box;
